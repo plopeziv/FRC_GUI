fb1d2f807b8fcde563f11a0bad3c3e537986aaa2
Pedro Lopez <plopeziv4@Pedros-MacBook-Pro.local>
2025-11-03 11:57:27 -0600

    Ability to upload excel files implemented

--- app.py
+++ app.py
@@ -6,7 +6,20 @@ Created on Mon Nov  3 11:15:08 2025
 """
 
 import streamlit as st
+import pandas as pd
 
-st.title("Hello, Streamlit!")
-st.write("This is your first Streamlit App")
-st.write("You can use streamlit to turn python to interactive maps")
+st.title("FRC Ticket GUI")
+st.write("Select Your FRC Ticket Listing Excel")
+
+excel_file = st.file_uploader("Choose Excel File", type=["xlsx", "xls"])
+
+if excel_file is not None:
+    try:
+        df = pd.read_excel(excel_file)
+
+        st.success("Load Successful!")
+
+    except Exception as e:
+        st.error(f"Error loading file: {e}")
+
+else: st.info("Please select a file to get started")
