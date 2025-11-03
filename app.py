30667da1ab1aa1ba0766bc19502949c9c8ec40fd
Pedro Lopez <plopeziv4@Pedros-MacBook-Pro.local>
2025-11-03 11:38:21 -0600

    Establish first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  3 11:15:08 2025
+
+@author: plopez
+"""
+
+import streamlit as st
+
+st.title("Hello, Streamlit!")
+st.write("This is your first Streamlit App")
+st.write("You can use streamlit to turn python to interactive maps")
