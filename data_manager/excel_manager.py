bd95df1a977f7587c4a1a277a43e619336736fb4
Pedro Lopez <plopeziv4@Pedros-MacBook-Pro.local>
2025-11-11 11:23:54 -0600

    Create a write to excel function

--- data_manager/excel_manager.py
+++ data_manager/excel_manager.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pathlib import Path
+from openpyxl import load_workbook
  
 
 class ExcelManager: 
@@ -76,14 +77,95 @@ class ExcelManager:
     def is_row_empty(self, row):
         return not any(bool(cell) for cell in row.fillna(0))
     
+    def insert_ticket (self, frc_ticket):
+        path = self.ensure_xlsx_copy(self.file_path)
+        ticket_data = frc_ticket
+        
+        wb=load_workbook(path)
+        ws = wb.active
+        
+        last_data_row_index = self.header_row + len(self.data_rows) + 1
+        new_row = last_data_row_index + 1
+        
+        self._insert_ticket_info(ws, new_row, ticket_data)
+        
+        
+        self._insert_labor(ws, new_row, ticket_data["Labor"])
+        
+        self._insert_materials(ws, new_row, ticket_data["Materials"])
+        
+        wb.save(self.file_path)
+        wb.close()
+        
+    def _safe_float(self, incoming_value):
+        try:
+            incoming_value = str(incoming_value).strip().lower()
+            return float(incoming_value) if incoming_value not in ("none", "", "nan") else None
+        except(ValueError, TypeError):
+            return None
+        
+    def _insert_ticket_info(self, worksheet, ticket_row, ticket_object):
+        worksheet.cell(row=ticket_row, column=7, value=ticket_object["Date"])
+        worksheet.cell(row=ticket_row, column=8, value=ticket_object["Signature"])
+        worksheet.cell(row=ticket_row, column=9, value=ticket_object["Ticket Number"])
+        worksheet.cell(row=ticket_row, column=10, value=ticket_object["Type"])
+        worksheet.cell(row=ticket_row, column=11, value=ticket_object["Description"])
+        
+    def _insert_labor(self, worksheet, ticket_row, labor_object):
+        worksheet.cell(row=ticket_row, column=19, value = self._safe_float(labor_object["RT"]))
+        worksheet.cell(row=ticket_row, column=20, value = self._safe_float(labor_object["OT"]))
+        worksheet.cell(row=ticket_row, column=21, value = self._safe_float(labor_object["DT"]))
+        worksheet.cell(row=ticket_row, column=22, value = self._safe_float(labor_object["OT DIFF"]))
+        worksheet.cell(row=ticket_row, column=23, value = self._safe_float(labor_object["DT DIFF"]))
+        
+    def _insert_materials(self, worksheet, material_row, materials):
+        for material_object in materials:
+            material_name = material_object["material"].strip()
+            material_quantity = self._safe_float(material_object["quantity"])
+                
+            try: 
+                column_index = self.headers.index(material_name) + 7
+                
+                worksheet.cell(row=material_row, column=column_index, value=material_quantity)
+                
+            except ValueError:
+                print(F"{material_name} not found in headers.... Skipped!")
+
+        
+        
+    def ensure_xlsx_copy(self, path):
+        """
+        If the working copy path is .xls, convert it to .xlsx for openpyxl.
+        Returns the new path (string) compatible with openpyxl.
+        """
+        path = Path(path)
+        if path.suffix.lower() == ".xls":
+            new_path = path.with_suffix(".xlsx")  # keep _test in the name already
+            if not new_path.exists():
+                print(f"⚙️ Converting {path.name} → {new_path.name}")
+                df = pd.read_excel(path, engine="xlrd", header=None)
+                df.to_excel(new_path, index=False, header=False)
+            return str(new_path)
+        return str(path)
+        
+        
+    
 if __name__ =="__main__":
-    test_path = "/Users/plopeziv4/Desktop/GUI Project/027386 - DETAILED TICKET LISTING.xls"
+    test_path = "027386 - DETAILED TICKET LISTING - PYTHON Copy Results.xlsx"
     
     manager = ExcelManager(test_path)
     
     df = manager.load()
     
-    rows = manager.data_rows
-    print(len(manager.data_rows[1]))
-    print(manager.get_materials())
+    incoming_ticket = {
+      'Ticket Number': '00001',
+      'Date': '11/10/25',
+      'Signature': 'Yes',
+      'Type': 'REGULAR',
+      'Description': 'Fixed pump housing leak',
+      'Labor': {'RT': '8', 'OT': '2', 'DT': '0', 'OT DIFF': '0.5', 'DT DIFF': '0'},
+      'Materials': [{'material': 'MAPEI PLANIPREP SC 10LB BAG', 'quantity': '3'}, {'material': 'MAPEI QUICK PATCH 25LB', 'quantity': '10'}]
+    }
+    
+    manager.insert_ticket(incoming_ticket)
         
