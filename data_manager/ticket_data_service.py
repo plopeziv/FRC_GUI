81994c75739fa98565975211eba570d9a5f6b1ae
Pedro Lopez <plopeziv4@Pedros-MacBook-Pro.local>
2025-11-12 14:26:30 -0600

    Allow Ticket Data Service to Build Labor and Material DBs

--- data_manager/ticket_data_service.py
+++ data_manager/ticket_data_service.py
@@ -1,15 +1,22 @@
 import pandas as pd
-from data_manager.excel_manager import ExcelManager
-# from excel_manager import ExcelManager
+# from data_manager.excel_manager import ExcelManager
+from excel_manager import ExcelManager
 
 class TicketDataService:
     def __init__(self, file_path=None):
         self.file_path = file_path
+        
         self.labor_summary = None
         self.labor_ticket_summary = None
+        
+        self.material_summary = None
+        self.material_ticket_summary = None
 
         self.excel_manager = ExcelManager(self.file_path)
+        
         self.excel_manager.load()
+        self.build_labor_data()
+        self.build_material_data()
         
     def build_labor_data(self):
         self._build_labor_summary()
@@ -73,13 +80,111 @@ class TicketDataService:
         df["Date"] = pd.to_datetime(df["Date"], errors="coerce").dt.strftime("%m/%d/%Y")
         
         self.labor_ticket_summary = df
+        
+    def build_material_data(self):
+        row5 = self.excel_manager.dataframe.iloc[5]
+        
+        col_index = row5[row5=="Structure Material #"].index[0]
+        col_pos = self.excel_manager.dataframe.columns.get_loc(col_index)
+        
+        self.material_summary = self._build_material_summary(col_pos)
+        
+        self.material_ticket_summary = self._build_material_ticket_summary(col_pos)
+        
+        # Update summary up to date columns
+        first_item = '1/4 UNDERLAYMENT 4 X 5"'
+        sliced_ticket_summary = self.material_ticket_summary.loc[:, first_item:]
+        
+        numeric_slice = sliced_ticket_summary.apply(pd.to_numeric, errors='coerce')
+        
+        numeric_totals = numeric_slice.sum()
+        
+        self.material_summary.loc["Material Counts to Date", numeric_totals.index] = numeric_totals.values
+        
+        cost_to_date_row = self.material_summary.loc["Material Counts to Date"] * self.material_summary.loc["Cost Per Unit (w/Tax)"]
+        self.material_summary.loc["Cost to Date"] = cost_to_date_row
+        
+        sell_to_date_row = self.material_summary.loc["Material Counts to Date"] * self.material_summary.loc["Sell Per Unit"]
+        self.material_summary.loc["Sell to Date"] = sell_to_date_row
+        
+        #Drop zero columns
+        
+        zero_columns = self.material_summary.loc["Material Counts to Date"] == 0
+
+        columns_to_drop = zero_columns[zero_columns].index.tolist()
+        
+        self.material_summary = self.material_summary.drop(columns=columns_to_drop)
+        self.material_ticket_summary = self.material_ticket_summary.drop(columns = columns_to_drop + ["Labor Sell", "Labor Cost"])
+        
+        # Complete Material Ticket Summary
+        first_filtered_item = self.material_summary.columns[0]
+        cost_per_unit = self.material_summary.loc["Cost Per Unit (w/Tax)", first_filtered_item:]
+        self.material_ticket_summary["Material Cost"] = (
+            self.material_ticket_summary.loc[:, first_filtered_item:].multiply(cost_per_unit, axis=1).sum(axis=1)
+        )
+        
+        sell_per_unit = self.material_summary.loc["Sell Per Unit", first_filtered_item:]
+        self.material_ticket_summary["Material Sell"] = (
+            self.material_ticket_summary.loc[:, first_filtered_item:].multiply(sell_per_unit, axis=1).sum(axis=1)
+        )
+        
+        return self.material_summary, self.material_ticket_summary
+        
+        
+    def _build_material_summary(self, col_pos):
+        df = self.excel_manager.dataframe.iloc[6:12, col_pos:]
+        df.columns = df.iloc[0]
+        df = df[1:]
+        df.reset_index(drop=True, inplace=True)
+        df.set_index(df.columns[0], inplace=True)
+        
+        return df
+    
+    def _build_material_ticket_summary(self, col_pos):
+        df = self.excel_manager.dataframe
+        
+        list_indices = [*range(0,9), *range(col_pos +1, len(df.columns))]
+        
+        material_ticket_df = df.iloc[13:, list_indices]
+        
+        start_pos = list_indices.index(col_pos + 1) 
+        
+        material_ticket_df = self._create_material_ticket_headers(material_ticket_df, col_pos, start_pos)
+        
+        
+        return material_ticket_df
+        
+    def _create_material_ticket_headers(self, df, col_pos, start_pos):
+        material_headers = self.excel_manager.dataframe.iloc[6, col_pos + 1 :].values
+        
+        df.iloc[0, start_pos:] = material_headers
+        
+        df.columns = df.iloc[0]
+        df = df[1:]
+        df.reset_index(drop=True, inplace=True)
+        df.set_index("Ticket #", inplace=True)
+        
+        return df
+        
+        
 
 
 if __name__ == "__main__":
     test_path = "027386 - DETAILED TICKET LISTING - PYTHON Copy.xlsx"
+    first_item = '1/4 UNDERLAYMENT 4 X 5"'
     
     manager = TicketDataService(test_path)
-    manager.build_labor_data()
     
-    df = manager.labor_summary
-    df2 = manager.labor_ticket_summary
+    material_summary, material_ticket_summary = manager.build_material_data()
+    
+    # material_ticket_summary = material_ticket_summary.loc[:, first_item:]
+    
+    # numeric_slice = material_ticket_summary.apply(pd.to_numeric, errors='coerce')
+    
+    # numeric_totals = numeric_slice.sum()
+    
+    # material_summary.loc["Material Counts to Date", numeric_totals.index] = numeric_totals.values
+    
+    
+    
+    # sum_of_material_categories = material_tikcet_df.loc[:, "RT":].sum().to_numpy()
