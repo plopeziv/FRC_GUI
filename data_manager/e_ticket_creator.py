896eff7b1023ebf5ecd712caf3911bfddb9f0d44
Pedro Lopez <plopez@flooringresourcescorp.com>
2025-12-01 16:28:24 -0600

    Get materials to add and save

--- data_manager/e_ticket_creator.py
+++ data_manager/e_ticket_creator.py
@@ -27,7 +27,12 @@ class ETicketCreator:
         ws = wb.active
         
         self._insert_job_info(ws)
+
         self._insert_labor(ws)
+        
+        self._insert_materials(ws)
+        
+        self._calculate_ticket_total(ws)
         
         wb.save(f"{self.incoming_ticket['Job Number']} - {self.incoming_ticket['Ticket Number']}.xlsx")
         
@@ -97,6 +102,16 @@ class ETicketCreator:
             in labor_object.items()
             if float(v["hours"]) > 0}
         
+        #Safety Check
+        if not active_labor:
+            rt_object = labor_object["RT"]
+            active_labor = {
+                    "REGULAR TIME": {
+                        "hours": "0",
+                        "rate": rt_object["rate"]
+                        }
+                }
+        
         iterator = iter(active_labor)
         first_key = next(iterator)
         
@@ -104,9 +119,7 @@ class ETicketCreator:
         ws['B17'] = round(float(active_labor[first_key]["hours"]), 1)
         ws['C17'] = first_key
         ws['F17'] = round(float(active_labor[first_key]["rate"]),2)
-        ws['I17'] = round(
-            float(active_labor[first_key]["hours"]) * float(active_labor[first_key]["rate"]), 2
-        )
+        ws['I17'] = "=B17*F17"
         
         # Insert Additional Labor Rows
         current_row = 17
@@ -122,9 +135,10 @@ class ETicketCreator:
            ws[f'B{current_row}'] = round(float(active_labor[key]["hours"]), 1)
            ws[f'C{current_row}'] = key
            ws[f'F{current_row}'] = round(float(active_labor[key]["rate"]),2)
-           ws[f'I{current_row}'] = round(
-               float(active_labor[key]["hours"]) * float(active_labor[key]["rate"]), 2
-           )
+           ws[f'I{current_row}'] = f'=B{current_row}*F{current_row}'
+           
+        ws[f'I{current_row+2}'] = f'=SUM(I17:I{current_row})'
+           
         
         
     def _copy_and_insert_row(self, worksheet, src_row, dest_row, max_col=9):
@@ -143,6 +157,75 @@ class ETicketCreator:
                 dest_cell.number_format = src_cell.number_format
                 dest_cell.protection = copy(src_cell.protection)
                 dest_cell.alignment = copy(src_cell.alignment)
+                
+    def _insert_materials(self, ws):
+        material_object = self.incoming_ticket["Materials"]
+        
+        if not material_object:
+           total_material_row = self._find_material_row(ws, "Total Material", column="G")
+           ws[f'I{total_material_row}'] = 0
+           return
+       
+        start_row = self._find_material_row(ws) + 1
+        
+        if start_row is None:
+            raise ValueError("Material starting row not found")
+            
+        # First Material Row
+        ws[f'B{start_row}'] = material_object[0]["quantity"]
+        ws[f'C{start_row}'] = material_object[0]["material"]
+        ws[f'F{start_row}'] = round(float(material_object[0]["sell price"]))
+        ws[f'I{start_row}'] = f'=B{start_row} * F{start_row}'
+        
+        ws.row_dimensions[start_row].height = 25
+        
+        ws.merge_cells(start_row=start_row, end_row=start_row, 
+                       start_column=3, end_column=4)
+      
+        # Any Material After 1
+        current_row = start_row
+        
+        for material in material_object[1:]:
+            current_row += 1
+            ws.insert_rows(current_row)
+            
+            self._copy_and_insert_row(ws, start_row, current_row)
+            
+            ws[f'B{current_row}'] = material["quantity"]
+            ws[f'C{current_row}'] = material["material"]
+            ws[f'F{current_row}'] = round(float(material["sell price"]))
+            ws[f'I{current_row}'] = f'=B{current_row} * F{current_row}'
+            
+            ws.row_dimensions[current_row].height = 25
+            
+            ws.merge_cells(start_row=current_row, end_row=current_row, 
+                           start_column=3, end_column=4)
+        
+        
+        # Create the material total summary
+        total_material_row = self._find_material_row(ws, "Total Material", column="G")
+        ws[f'I{total_material_row}'] = f'=SUM(I{start_row}:I{current_row})'
+        
+        
+        
+    def _calculate_ticket_total(self, ws):
+        labor_total_row = self._find_material_row(ws, "Total Hours", column='G')
+        material_total_row = self._find_material_row(ws, "Total Material", column='G')
+        ticket_total_row = self._find_material_row(ws, "Total Ticket", column='G')
+        
+        formula = f'=I{labor_total_row}+I{material_total_row}'
+        ws[f'I{ticket_total_row}'] = formula
+        ws.row_dimensions[ticket_total_row].height = 15
+        
+
+        
+    def _find_material_row(self, ws, search_term="Material Used:", column="A"):
+        for row in range(1,50):
+            check_value = ws[f'{column}{row}'].value
+            if check_value and search_term in str(check_value):
+                return row
+        
+        return None    
     
         
         
@@ -168,21 +251,29 @@ if __name__ =="__main__":
       'Work Location': '35TH FLR',
       'Description': 'Fixed pump housing leak',
       'Labor': {
-          'RT': {'hours':'8', 'rate': '157.15'}, 
-          'OT': {'hours':'2', 'rate': '197.70'}, 
+          'RT': {'hours':'12', 'rate': '153.15'}, 
+          'OT': {'hours':'4', 'rate': '197.70'}, 
           'DT': {'hours':'0', 'rate': '237.72'}, 
-          'OT DIFF': {'hours':'0.5', 'rate':'40.55'}, 
+          'OT DIFF': {'hours':'0', 'rate':'40.55'}, 
           'DT DIFF': {'hours': '0', 'rate': '80.57'}
           },
       'Materials': [
           {
               'material': 'MAPEI PLANIPREP SC 10LB BAG', 
-              'quantity': '3'
+              'quantity': '3',
+              'sell price': '32.50'
           }, 
           {
               'material': 'MAPEI QUICK PATCH 25LB', 
-              'quantity': '10'
-          }
+              'quantity': '10',
+              'sell price': '36.20'
+          },
+          {
+              'material': 'HEPA SANDER/VAC', 
+              'quantity': '1',
+              'sell price': '150'
+          }, 
+
      ]
     }
     
